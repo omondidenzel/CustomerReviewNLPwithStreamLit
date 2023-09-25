8c53a1e8572e17a662706782eeb70d1f3db57033
DENZEL OMONDI <omondidenzel@DENZELs-MacBook-Pro.local>
2023-09-25 18:49:56 +0300

    first-commit

---
+++ App.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import pickle
+import pandas as pd 
+
+st.header("Customer review predictor" ,divider='rainbow')
+
+st.write("""This apps predicts **reviews** by customers.""")
+
+review = st.text_input("Review input","Type in your review")
+
+#Load model
+file_name = "NLP/LogisticRegressionModel.pkl"
+loadfile = open(file_name, 'rb')
+loadedModel = pickle.load(loadfile)
+value = loadedModel.predict([review])
+
+
+st.subheader('Prediction')
+st.write(value)
